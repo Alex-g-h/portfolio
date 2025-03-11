ebb094f4974ed8e4096472326af0261e6301103a
Aleksey Bulygin <alex987@bk.ru>
2025-03-11 17:50:54 +0300

    Snoop Mini

--- src/app/data/projectsData.js
+++ src/app/data/projectsData.js
@@ -1,6 +1,38 @@
 export const ProjectsData = [
   {
     id: 1,
+    name: 'Snoop mini',
+    description:
+      'Snoop mini — мобильное приложение компаньон, предназначенное для работы с устройством обнаружения утечек сигналов Arcom Digital Snoop Mini. Приложение позволяет конфигурировать и управлять процессом обнаружения утечек сигналов в кабельной сети. ',
+    link: {
+      href: 'https://drive.google.com/drive/folders/1Gr3VRhrHNm57GsXdrV4UORxo9RsdN00y',
+      text: 'Видео применения',
+    },
+    imgUrls: [
+      'assets/images/projects/snoop-mini/sm1.jpg',
+      'assets/images/projects/snoop-mini/sm2.jpg',
+      'assets/images/projects/snoop-mini/sm3.jpg',
+      'assets/images/projects/snoop-mini/sm4.jpg',
+      'assets/images/projects/snoop-mini/sm5.jpg',
+      'assets/images/projects/snoop-mini/sm6.jpg',
+      'assets/images/projects/snoop-mini/sm7.jpg',
+    ],
+    tags: [
+      'Flutter',
+      'Dart',
+      'Bluetooth',
+      'BLE',
+      'FFI',
+      'Geocoding',
+      'Android',
+      'iOS',
+    ],
+    googlePlay:
+      'https://play.google.com/store/apps/details?id=com.arcomdigital.snoop_bt',
+    appStore: 'https://apps.apple.com/app/snoop-mini/id6742740860',
+  },
+  {
+    id: 2,
     name: 'Quiver QC',
     description:
       'Quiver QC — мобильное приложение, которое позволяет реализовать метрику контроля качества (QC) при использовании полевого измерителя Arcom Digital Quiver. Приложение предоставляет простой способ загрузки сохраненных снимков экрана Quiver, документирующих условия до и после ремонта для различных неисправностей. Приложение обращается к камере мобильного телефона, сканирует QR-код, отображаемый на экране Quiver, представляющий снимок экрана Quiver, преобразует захваченный QR-код обратно в снимок экрана, а затем загружает снимки экрана на сервер Cloud QC вместе с координатами и метаданными для анализа и использования менеджерами. При отсутствии связи отправку результатов можно отложить.',
@@ -39,7 +71,7 @@ export const ProjectsData = [
     appStore: 'https://apps.apple.com/app/quiver-qc/id6648794270',
   },
   {
-    id: 2,
+    id: 3,
     name: 'Blackbird',
     description:
       'Продукт представляющий собой плату Blackbird и Raspberry Pi, соединенные между собою. На плате имеется микроконтроллер (MCU) и ПЛИС (FPGA). MCU взаимодействует с Raspberry Pi через протокол SPI и GPIO. На Raspberry Pi развернуто Full Stack SPA приложение. Решение позволяет обнаруживать утечки сигнала в разных частотных диапазонах (Aero, High) при разных типах модуляции/режимах (OFDM, Pilot/QAM, OFDM Upstream Data Profile (OUDP), Full duplex (FDX)). Решение также позволяет анализировать спектры. С помощью WEB приложения можно обновлять прошивки для MCU, FPGA.',
@@ -73,7 +105,7 @@ export const ProjectsData = [
     ],
   },
   {
-    id: 3,
+    id: 4,
     name: 'Gains vs Spending',
     description:
       'Демо Full Stack SPA приложение учета доходов и расходов на стеке MERN. Имеется авторизация, набор банковских счетов для большинства платежных систем, связанная с ними таблица доходов и таблица трат. Раздел аналитики доходов и расходов с графиками и статистикой.',
@@ -99,7 +131,7 @@ export const ProjectsData = [
     url: 'http://5.53.124.22/gains-vs-spending',
   },
   {
-    id: 4,
+    id: 5,
     name: 'Fast Company',
     description:
       'Демо Full Stack SPA приложение знакомств на стеке MERN. Имеется авторизация, личная страница с характеристиками. Зарегистрированные пользователи могут увидеть анкеты всех прочих пользователей, зарегистрировавшихся ранее, а также оставить комментарии на страницах других пользователей. Поддерживается функционал редактирования созданных учётных данных. Страница с пользователями поддерживает сортировку по имеющимся полям (столбцам) и фильтрацию по профессиям (отдельное меню слева).',
@@ -127,7 +159,7 @@ export const ProjectsData = [
     url: 'https://alex-g-h.github.io/fast-company/',
   },
   {
-    id: 5,
+    id: 6,
     name: 'Metal Company',
     description: 'Landing page компании металлопроката.',
     imgUrls: [
